11188224e7e15444d334b6e92172eee3873e1e7c
Thyago Santos <thyagomatheus15@gmail.com>
2025-12-19 23:45:19 -0300

    Atualizar o game.js

--- src/game.js
+++ src/game.js
@@ -1,24 +1,13 @@
-
-/**
-* The core EndlessRunner blueprint game file.
-* 
-* This file is only used to initalise (start-up) the main Kiwi Game 
-* and add all of the relevant states to that Game.
-*
-*/
-
-//Initialise the Kiwi Game. 
+// src/game.js
 var gameOptions = {
-	width: 1210,
-	height: 1550
-	
-}
+    width: 1210,
+    height: 1550
+};
 
 var game = new Kiwi.Game('content', 'Match3', null, gameOptions);
 
-//Add all the States we are going to use.
 game.states.addState(LoadingState);
-game.states.addState(IntroState);
 game.states.addState(PlayState);
+// game.states.addState(IntroState);  // Comente se não usar
 
 game.states.switchState("LoadingState");
